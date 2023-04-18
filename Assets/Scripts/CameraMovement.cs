ee09506bb0167aca5e05d1e46878ee0aa4ebc32f
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-04-18 16:41:50 +0200

    Add CameraMovement Script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraMovement:MonoBehaviour {
+    private float xRotation;
+    private float yRotation;
+    [SerializeField] private float sensitivity; //Make global for settings later
+    [SerializeField] private Transform person;
+
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //x-axis controls up and down rotation, y-axis controls left and right rotation.
+        yRotation += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+        //Rotates the body sideways.
+        person.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
