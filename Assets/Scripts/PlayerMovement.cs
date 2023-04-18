cde405d1248754adffaa1dd890260cf469883a49
Kenan Dizdarevic <kenandi@kth.se>
2023-04-18 16:11:48 +0200

    Add Time.fixedDeltaTime

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,10 @@ public class PlayerMovement:MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    /* Update is called once per frame, used when you want to check for input.
+     * FixedUpdate is called once per physics update, used when you want to move the player.
+     * Ground check is supposed to be in Update, want to check it every frame. 
+     */
     void Update() {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     }
@@ -29,6 +32,6 @@ public class PlayerMovement:MonoBehaviour {
     /// </summary>
     /// <param name="direction">Direction of the player as a <see cref="Vector3"/></param>
     void movePlayer(Vector3 direction) {
-        rb.velocity = direction * walkSpeed;
+        rb.velocity = Time.deltaTime * walkSpeed * direction;
     }
 }
