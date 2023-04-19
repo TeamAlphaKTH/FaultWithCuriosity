8c30247080eea7618525cdaa69545978fdf8dd5b
Kenan Dizdarevic <kenandi@kth.se>
2023-04-19 16:39:08 +0200

    Add slower diagonal movement and deceleration
    Slower sideways, backward and diagonal movement update. Using pythagoran theorem to calculate correct diagonal speed. Add new movement parameters: slowSpeedWalk and slowSpeedRun.
    
    Add deceleration with use of Vector2.MoveTowards().

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -8,8 +8,13 @@ public class FirstPersonController:MonoBehaviour {
     [SerializeField] private float walkSpeed = 3.0f;
     [SerializeField] private float runSpeed = 6.0f;
 
+    // Movement speed when the player is not moving forward.
+    [SerializeField] private float slowSpeedWalk = 1.8f;
+    [SerializeField] private float slowSpeedRun = 3.6f;
+
     // Lower values will make the character accelerate slower, higher values will make the character accelerate faster.
     [SerializeField] private float acceleration = 100f;
+    [SerializeField] private float deceleration = 100f;
 
     [Header("Jumping Parameters")]
     [SerializeField] private float gravity = 30f;
@@ -24,9 +29,12 @@ public class FirstPersonController:MonoBehaviour {
     [SerializeField] private KeyCode runKey = KeyCode.LeftShift;
 
     private Vector2 currentInput;
+
+    // Initialize currentSpeed to zero so that the character doesn't move when the game starts.
     private Vector2 currentSpeed = Vector2.zero;
     private Vector3 moveDirection;
     private CharacterController characterController;
+
 
     // Start is called before the first frame update
     void Start() {
@@ -58,19 +66,45 @@ public class FirstPersonController:MonoBehaviour {
     /// </summary>
     /// <remarks>
     /// Calculates the movement direction based on the player's input and sets the character's velocity accordingly.
+    /// Lets the player accelerate when it wants to move. When no input is given, the character will stop moving.
     /// </remarks>
     private void HandleInput() {
+        // If the player is running, the walk speed is set to the run speed, otherwise it is set to the walk speed.
+        float baseSpeed = IsRunning ? runSpeed : walkSpeed;
+        float speed = baseSpeed;
 
-        float speed = IsRunning ? runSpeed : walkSpeed;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        float accelerationRate = horizontalInput == 0 && verticalInput == 0 ? deceleration : acceleration;
+
+        // If the player is not moving forward, use slower speed.
+        if(horizontalInput == 0 || verticalInput == 0) {
+            if(horizontalInput < 0 || horizontalInput > 0 || verticalInput < 0) {
+                // Moving backwards or sideways, use slower speed.
+                speed = IsRunning ? slowSpeedRun : slowSpeedWalk;
+            } else {
+                speed = baseSpeed;
+            }
+        } else {
+            // Moving diagonally, use slower speed, calculated with pythagorean theorem.
+            float diagonalSpeed = Mathf.Sqrt(baseSpeed * baseSpeed / 2f);
+
+            if(verticalInput < 0) {
+                // Moving diagonally backward, use slower diagonal speed.
+                speed = IsRunning ? slowSpeedRun * diagonalSpeed / baseSpeed : slowSpeedWalk * diagonalSpeed / baseSpeed;
+            } else {
+                // Moving diagonally forward, use normal diagonal speed.
+                speed = diagonalSpeed;
+            }
+        }
+
 
         // 2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
-        currentInput = new Vector2(speed * Input.GetAxisRaw("Vertical"), speed * Input.GetAxisRaw("Horizontal"));
+        currentInput = new Vector2(speed * verticalInput, speed * horizontalInput);
 
-        if(currentInput == Vector2.zero) {
-            currentSpeed = Vector2.zero;
-        } else {
-            currentSpeed = Vector2.MoveTowards(currentSpeed, currentInput, acceleration * Time.deltaTime);
-        }
+        currentSpeed = Vector2.MoveTowards(currentSpeed, currentInput, accelerationRate * Time.deltaTime);
+
         Debug.Log("Current speed: " + currentSpeed);
         float moveDirectionY = moveDirection.y;
 
