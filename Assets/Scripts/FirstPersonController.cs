19c5d575dd7af10ed8f1f06d15b2ea4ec30e4c49
akessonalfred <102586522+akessonalfred@users.noreply.github.com>
2023-04-19 10:31:07 +0100

    Add walk for player (#16)
    * Add walk for player
    
    Add walk functionality to navigate the world with the help of character controller
    
    * Fix canMove to CanMove

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class FirstPersonController : MonoBehaviour
-{
-    public bool canMove { get; private set; } = true;
-	[SerializeField] private CharacterController characterController;
+public class FirstPersonController:MonoBehaviour {
+    public bool CanMove { get; private set; } = true;
 
-	[Header("Jumping Parameters")]
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float runSpeed = 10.0f;
+
+    [Header("Jumping Parameters")]
     [SerializeField] private float gravity = 30f;
     [SerializeField] private float jumpForce = 8f;
 
@@ -18,41 +17,60 @@ public class FirstPersonController : MonoBehaviour
     [Header("Controls")]
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
+    private Vector2 currentInput;
     private Vector3 moveDirection;
+    private CharacterController characterController;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(canMove) {
-			HandleJump();
-			ApplyFinalMovement();
-		}
+    void Update() {
+        if(CanMove) {
+            HandleInput();
+            HandleJump();
+            ApplyFinalMovement();
+        }
     }
 
     /// <summary>
     /// Handles jump
     /// </summary>
     private void HandleJump() {
-        if (canJump) {
-			if(Input.GetKey(jumpKey) && characterController.isGrounded) {
-				moveDirection.y = jumpForce;
-			}
-		}
+        if(canJump) {
+            if(Input.GetKey(jumpKey) && characterController.isGrounded) {
+                moveDirection.y = jumpForce;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles the input for the character movement.
+    /// </summary>
+    /// <remarks>
+    /// Calculates the movement direction based on the player's input and sets the character's velocity accordingly.
+    /// </remarks>
+    void HandleInput() {
+
+        //2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
+        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+
+        //Calculates the movement direction of the character based on the current input vector and the orientation of the character in the world.
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
     }
 
     /// <summary>
     /// Applies movement to player, depending on positional values
     /// </summary>
     private void ApplyFinalMovement() {
-		if(!characterController.isGrounded) {
-			moveDirection.y -= gravity * Time.deltaTime;
-		}
-		characterController.Move(moveDirection * Time.deltaTime);
-	}
+        if(!characterController.isGrounded) {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
 }
