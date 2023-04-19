c337ed2b2e328e967ed6972530e62a4a2838bc3e
Kenan Dizdarevic <kenandi@kth.se>
2023-04-19 13:50:37 +0200

    Add run for player in FirstPersonController.cs
    Add run by utilizing Vector2 for calculating current speed. This makes it possible for the player to accelerate up to a certain speed. When no input is detected the player will instantly stop.
    
    Setting a higher acceleration value will make the acceleration go faster due to the multiplication with Time.deltaTime.

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class FirstPersonController:MonoBehaviour {
     public bool CanMove { get; private set; } = true;
+    private bool IsRunning => Input.GetKey(runKey) && canRun;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
-    [SerializeField] private float runSpeed = 10.0f;
+    [SerializeField] private float runSpeed = 6.0f;
+
+    // Lower values will make the character accelerate slower, higher values will make the character accelerate faster.
+    [SerializeField] private float acceleration = 100f;
 
     [Header("Jumping Parameters")]
     [SerializeField] private float gravity = 30f;
@@ -13,11 +17,14 @@ public class FirstPersonController:MonoBehaviour {
 
     [Header("Functional Options")]
     [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canRun = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode runKey = KeyCode.LeftShift;
 
     private Vector2 currentInput;
+    private Vector2 currentSpeed = Vector2.zero;
     private Vector3 moveDirection;
     private CharacterController characterController;
 
@@ -52,15 +59,23 @@ public class FirstPersonController:MonoBehaviour {
     /// <remarks>
     /// Calculates the movement direction based on the player's input and sets the character's velocity accordingly.
     /// </remarks>
-    void HandleInput() {
+    private void HandleInput() {
 
-        //2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        float speed = IsRunning ? runSpeed : walkSpeed;
 
+        // 2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
+        currentInput = new Vector2(speed * Input.GetAxisRaw("Vertical"), speed * Input.GetAxisRaw("Horizontal"));
+
+        if(currentInput == Vector2.zero) {
+            currentSpeed = Vector2.zero;
+        } else {
+            currentSpeed = Vector2.MoveTowards(currentSpeed, currentInput, acceleration * Time.deltaTime);
+        }
+        Debug.Log("Current speed: " + currentSpeed);
         float moveDirectionY = moveDirection.y;
 
-        //Calculates the movement direction of the character based on the current input vector and the orientation of the character in the world.
-        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        // Calculates the movement direction of the character based on the current input vector and the orientation of the character in the world.
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentSpeed.x) + (transform.TransformDirection(Vector3.right) * currentSpeed.y);
         moveDirection.y = moveDirectionY;
     }
 
