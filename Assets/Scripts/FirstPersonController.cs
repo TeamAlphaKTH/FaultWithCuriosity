a6e2d1140db2c09466c5fccaf8796410e0a9ad6e
Kenan Dizdarevic <99963700+kenandizdarevic@users.noreply.github.com>
2023-04-20 08:36:18 +0200

    Add slower diagonal movement and deceleration (#17)
    * Add run for player in FirstPersonController.cs
    
    Add run by utilizing Vector2 for calculating current speed. This makes it possible for the player to accelerate up to a certain speed. When no input is detected the player will instantly stop.
    
    Setting a higher acceleration value will make the acceleration go faster due to the multiplication with Time.deltaTime.
    
    * Add slower diagonal movement and deceleration
    
    Slower sideways, backward and diagonal movement update. Using pythagoran theorem to calculate correct diagonal speed. Add new movement parameters: slowSpeedWalk and slowSpeedRun.
    
    Add deceleration with use of Vector2.MoveTowards().

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,127 +1,174 @@
 using UnityEngine;
 
 public class FirstPersonController:MonoBehaviour {
-	public bool CanMove { get; private set; } = true;
+    public bool CanMove { get; private set; } = true;
+    private bool IsRunning => Input.GetKey(runKey) && canRun;
 
-	[Header("Movement Parameters")]
-	[SerializeField] private float walkSpeed = 3.0f;
-	[SerializeField] private float runSpeed = 10.0f;
-	[SerializeField] private float climbSpeed = 3.0f;
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float runSpeed = 6.0f;
+    [SerializeField] private float climbSpeed = 3.0f;
 
-	[Header("Jumping Parameters")]
-	[SerializeField] private float gravity = 30f;
-	[SerializeField] private float jumpForce = 8f;
+    // Movement speed when the player is not moving forward.
+    [SerializeField] private float slowSpeedWalk = 1.8f;
+    [SerializeField] private float slowSpeedRun = 3.6f;
 
-	[Header("Functional Options")]
-	[SerializeField] private bool canJump = true;
+    // Lower values will make the character accelerate slower, higher values will make the character accelerate faster.
+    [SerializeField] private float acceleration = 100f;
+    [SerializeField] private float deceleration = 100f;
 
-	[Header("Controls")]
-	[SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [Header("Jumping Parameters")]
+    [SerializeField] private float gravity = 30f;
+    [SerializeField] private float jumpForce = 8f;
 
-	private Vector2 currentInput;
-	private Vector3 moveDirection;
-	private CharacterController characterController;
-	private float oldGravity;
+    [Header("Functional Options")]
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canRun = true;
 
-	// Start is called before the first frame update
-	void Start() {
-		characterController = GetComponent<CharacterController>();
-	}
+    [Header("Controls")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode runKey = KeyCode.LeftShift;
 
-	// Update is called once per frame
-	void Update() {
-		if(CanMove) {
-			HandleInput();
-			HandleJump();
-			ApplyFinalMovement();
-		}
-	}
+    private Vector2 currentInput;
 
-	/// <summary>
-	/// Handles jump
-	/// </summary>
-	private void HandleJump() {
-		if(canJump) {
-			if(Input.GetKey(jumpKey) && characterController.isGrounded) {
-				moveDirection.y = jumpForce;
-			}
-		}
-	}
+    // Initialize currentSpeed to zero so that the character doesn't move when the game starts.
+    private Vector2 currentSpeed = Vector2.zero;
+    private Vector3 moveDirection;
+    private CharacterController characterController;
+    private float oldGravity;
 
-	/// <summary>
-	/// Handles the input for the character movement.
-	/// </summary>
-	/// <remarks>
-	/// Calculates the movement direction based on the player's input and sets the character's velocity accordingly.
-	/// </remarks>
-	void HandleInput() {
-		//2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
-		currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+    // Start is called before the first frame update
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
 
-		float moveDirectionY = moveDirection.y;
+    // Update is called once per frame
+    void Update() {
+        if(CanMove) {
+            HandleInput();
+            HandleJump();
+            ApplyFinalMovement();
+        }
+    }
 
-		//Calculates the movement direction of the character based on the current input vector and the orientation of the character in the world.
-		moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
-		moveDirection.y = moveDirectionY;
-	}
+    /// <summary>
+    /// Handles jump
+    /// </summary>
+    private void HandleJump() {
+        if(canJump) {
+            if(Input.GetKey(jumpKey) && characterController.isGrounded) {
+                moveDirection.y = jumpForce;
+            }
+        }
+    }
 
-	/// <summary>
-	/// Applies movement to player, depending on positional values
-	/// </summary>
-	private void ApplyFinalMovement() {
-		if(!characterController.isGrounded) {
-			moveDirection.y -= gravity * Time.deltaTime;
-		}
-		characterController.Move(moveDirection * Time.deltaTime);
-	}
+    /// <summary>
+    /// Handles the input for the character movement.
+    /// </summary>
+    /// <remarks>
+    /// Calculates the movement direction based on the player's input and sets the character's velocity accordingly.
+    /// Lets the player accelerate when it wants to move. When no input is given, the character will stop moving.
+    /// </remarks>
+    private void HandleInput() {
+        // If the player is running, the walk speed is set to the run speed, otherwise it is set to the walk speed.
+        float baseSpeed = IsRunning ? runSpeed : walkSpeed;
+        float speed = baseSpeed;
 
-	/// <summary>
-	/// Handles player on ladder going up down left and right 
-	/// </summary>
-	/// <param name="ladderHitbox"></param>
-	private void OnTriggerStay(Collider ladderHitbox) {
-		//Check that the gameobject is a "Ladder"
-		if(ladderHitbox.CompareTag("Ladder")) {
-			//Turn off gravity and normal movement while on "Ladder"
-			if(gravity != 0f)
-				oldGravity = gravity;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-			gravity = 0f;
+        float accelerationRate = horizontalInput == 0 && verticalInput == 0 ? deceleration : acceleration;
 
-			if(!characterController.isGrounded) {
-				CanMove = false;
-			} else {
-				CanMove = true;
-			}
+        // If the player is not moving forward, use slower speed.
+        if(horizontalInput == 0 || verticalInput == 0) {
+            if(horizontalInput < 0 || horizontalInput > 0 || verticalInput < 0) {
+                // Moving backwards or sideways, use slower speed.
+                speed = IsRunning ? slowSpeedRun : slowSpeedWalk;
+            } else {
+                speed = baseSpeed;
+            }
+        } else {
+            // Moving diagonally, use slower speed, calculated with pythagorean theorem.
+            float diagonalSpeed = Mathf.Sqrt(baseSpeed * baseSpeed / 2f);
 
-			//Handles movement on "Ladder"
-			if(Input.GetAxis("Vertical") > 0) {
-				Debug.Log(Input.GetAxis("Vertical"));
-				moveDirection = (transform.TransformDirection(Vector3.up) * climbSpeed);
-				characterController.Move(moveDirection * Time.deltaTime);
-			} else if(Input.GetAxis("Vertical") < 0) {
-				moveDirection = (transform.TransformDirection(Vector3.down) * climbSpeed);
-				characterController.Move(moveDirection * Time.deltaTime);
-			}
-			if(Input.GetAxis("Horizontal") > 0) {
-				moveDirection = (transform.TransformDirection(Vector3.right) * climbSpeed);
-				characterController.Move(moveDirection * Time.deltaTime);
-			} else if(Input.GetAxis("Horizontal") < 0) {
-				moveDirection = (transform.TransformDirection(Vector3.left) * climbSpeed);
-				characterController.Move(moveDirection * Time.deltaTime);
-			}
-		}
-	}
+            if(verticalInput < 0) {
+                // Moving diagonally backward, use slower diagonal speed.
+                speed = IsRunning ? slowSpeedRun * diagonalSpeed / baseSpeed : slowSpeedWalk * diagonalSpeed / baseSpeed;
+            } else {
+                // Moving diagonally forward, use normal diagonal speed.
+                speed = diagonalSpeed;
+            }
+        }
 
-	/// <summary>
-	/// Handles when player leave ladder
-	/// </summary>
-	/// <param name="ladderHitbox"></param>
-	private void OnTriggerExit(Collider ladderHitbox) {
-		if(ladderHitbox.CompareTag("Ladder")) {
-			//Activates normal movement and gravity
-			CanMove = true;
-			gravity = oldGravity;
-		}
-	}
+        // 2D vector based on the player's input axis for vertical and horizontal movement and scales it by walk speed.
+        currentInput = new Vector2(speed * verticalInput, speed * horizontalInput);
+
+        currentSpeed = Vector2.MoveTowards(currentSpeed, currentInput, accelerationRate * Time.deltaTime);
+        Debug.Log("Current speed: " + currentSpeed);
+        float moveDirectionY = moveDirection.y;
+
+        // Calculates the movement direction of the character based on the current input vector and the orientation of the character in the world.
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentSpeed.x) + (transform.TransformDirection(Vector3.right) * currentSpeed.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    /// <summary>
+    /// Applies movement to player, depending on positional values
+    /// </summary>
+    private void ApplyFinalMovement() {
+        if(!characterController.isGrounded) {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Handles player on ladder going up down left and right 
+    /// </summary>
+    /// <param name="ladderHitbox"></param>
+    private void OnTriggerStay(Collider ladderHitbox) {
+        //Check that the gameobject is a "Ladder"
+        if(ladderHitbox.CompareTag("Ladder")) {
+            //Turn off gravity and normal movement while on "Ladder"
+            if(gravity != 0f)
+                oldGravity = gravity;
+
+            gravity = 0f;
+
+            if(!characterController.isGrounded) {
+                CanMove = false;
+            } else {
+                CanMove = true;
+            }
+
+            //Handles movement on "Ladder"
+            if(Input.GetAxis("Vertical") > 0) {
+                Debug.Log(Input.GetAxis("Vertical"));
+                moveDirection = (transform.TransformDirection(Vector3.up) * climbSpeed);
+                characterController.Move(moveDirection * Time.deltaTime);
+            } else if(Input.GetAxis("Vertical") < 0) {
+                moveDirection = (transform.TransformDirection(Vector3.down) * climbSpeed);
+                characterController.Move(moveDirection * Time.deltaTime);
+            }
+            if(Input.GetAxis("Horizontal") > 0) {
+                moveDirection = (transform.TransformDirection(Vector3.right) * climbSpeed);
+                characterController.Move(moveDirection * Time.deltaTime);
+            } else if(Input.GetAxis("Horizontal") < 0) {
+                moveDirection = (transform.TransformDirection(Vector3.left) * climbSpeed);
+                characterController.Move(moveDirection * Time.deltaTime);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles when player leave ladder
+    /// </summary>
+    /// <param name="ladderHitbox"></param>
+    private void OnTriggerExit(Collider ladderHitbox) {
+        if(ladderHitbox.CompareTag("Ladder")) {
+            //Activates normal movement and gravity
+            CanMove = true;
+            gravity = oldGravity;
+        }
+    }
 }
