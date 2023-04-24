f152e7739e0e930e3486ca5ea5477ca83782dec2
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-04-24 15:53:07 +0200

    Add Inventory and script

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,72 +3,82 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory:MonoBehaviour {
-    [SerializeField] private KeyCode openInventory = KeyCode.Tab;
-    [SerializeField] private GameObject inventory = GameObject.Find("Inventory Background");
-    [SerializeField] private TextMeshProUGUI drugText;
-    private int drugNr;
+	[SerializeField] private KeyCode openInventory = KeyCode.Tab;
+	[SerializeField] private GameObject inventory;
+	[SerializeField] private TextMeshProUGUI drugText;
+	private int drugNr;
 
-    [SerializeField] private TextMeshProUGUI batteryText;
-    private int batteryNr;
-    [SerializeField] private Slider flashlightSlider;
-    [SerializeField] private Slider cameraSlider;
-    private void Start() {
+	[SerializeField] private TextMeshProUGUI batteryText;
+	private int batteryNr;
+	[SerializeField] private Slider flashlightSlider;
+	[SerializeField] private Slider cameraSlider;
 
-    }
-    // Update is called once per frame
-    void Update() {
-        if(Input.GetKeyDown(openInventory)) {
-            switch(inventory.activeSelf) {
-                case true:
-                inventory.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked;
-                FirstPersonController.CanMove = true;
-                CameraMovement.CanRotate = true;
+	private void Start() {
+		flashlightSlider.value = Flashlight.batteryLevel;
+	}
 
-                break;
-                case false:
-                inventory.SetActive(true);
-                Cursor.lockState = CursorLockMode.Confined;
-                FirstPersonController.CanMove = false;
-                CameraMovement.CanRotate = false;
-                break;
-            }
-        }
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetKeyDown(openInventory)) {
+			switch(inventory.activeSelf) {
+				case true:
+				inventory.SetActive(false);
+				Cursor.lockState = CursorLockMode.Locked;
+				FirstPersonController.CanMove = true;
+				CameraMovement.CanRotate = true;
 
+				break;
+				case false:
+				inventory.SetActive(true);
+				Cursor.lockState = CursorLockMode.Confined;
+				FirstPersonController.CanMove = false;
+				CameraMovement.CanRotate = false;
+				break;
+			}
+		}
 
-    }
+		flashlightSlider.value = Flashlight.batteryLevel;
+	}
 
-    public void UseDrugs() {
+	public void UseDrugs() {
+		drugNr = int.Parse(drugText.text);
+		if(drugNr > 0) {
+			drugNr--;
+			drugText.text = drugNr.ToString();
+		}
+	}
 
-        drugNr = int.Parse(drugText.text);
-        if(drugNr > 0) {
-            drugNr--;
-            drugText.text = drugNr.ToString();
-        }
-    }
-    public void UseCamera() {
-        batteryNr = int.Parse(batteryText.text);
-        if(batteryNr > 0) {
-            batteryNr--;
-            batteryText.text = batteryNr.ToString();
-            cameraSlider.value++;
-        }
+	public void DropDrugs() {
+		drugNr = int.Parse(drugText.text);
+		if(drugNr > 0) {
+			drugNr--;
+			drugText.text = drugNr.ToString();
+		}
+	}
 
-    }
-    public void UseFlashlight() {
-        batteryNr = int.Parse(batteryText.text);
-        if(batteryNr > 0) {
-            batteryNr--;
-            batteryText.text = batteryNr.ToString();
-            flashlightSlider.value++;
-        }
+	public void RechargeCamera() {
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0 && cameraSlider.value < 3) {
+			batteryNr--;
+			batteryText.text = batteryNr.ToString();
+			cameraSlider.value++;
+		}
 
-    }
-    public void DropBattery() {
-        batteryNr = int.Parse(batteryText.text);
-        if(batteryNr > 0) {
-            batteryNr--;
-            batteryText.text = batteryNr.ToString();
-        }
-    }
+	}
+	public void RechargeFlashlight() {
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0 && Flashlight.batteryLevel != 100) {
+			batteryNr--;
+			batteryText.text = batteryNr.ToString();
+			Flashlight.batteryLevel = Flashlight.batteryLevel >= 80 ? 100 : Flashlight.batteryLevel += 20;
+		}
+
+	}
+	public void DropBattery() {
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0) {
+			batteryNr--;
+			batteryText.text = batteryNr.ToString();
+		}
+	}
 }
