2d4955f711e3f28485c20b07f3cd1ca9bcbba17f
AbudZoughbi <119438189+AbudZoughbi@users.noreply.github.com>
2023-04-24 13:44:40 +0200

    Add camera, flash, polaroid
    Add camera that flashes and outputs a polaroid picture.

---
+++ Assets/Scripts/ItemCamera.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCamera : MonoBehaviour
+{
+    [Header("Camera Item Parameters")]
+    [SerializeField] private int charges = 3;
+    [SerializeField] private KeyCode useCameraButton = KeyCode.Mouse1;
+    [SerializeField] private KeyCode rechargeCameraButton = KeyCode.Mouse2;
+	[SerializeField] private bool canRechargeCamera;
+    public static bool canUseCamera = true;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canUseCamera = charges > 0 ? true : false;
+		UseCamera();
+		canRechargeCamera = charges >= 3 ? false : true;
+		RechargeCamera();
+    }
+
+    /// <summary>
+    /// Method for using camera if the button is pressed and player canUseCamera is true,
+    /// charges exceed 0
+    /// </summary>
+    private void UseCamera() {
+        if(Input.GetKeyDown(useCameraButton)) {
+            if(canUseCamera) {
+                charges--;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method for recharging camera by testing if there is a battery and it is used on the camera
+    /// also that the charges does not exceed 3
+    /// </summary>
+    private void RechargeCamera() {
+        if(Input.GetKeyDown(rechargeCameraButton)) {
+            // Requires to have a battery in the future and its use on camera
+            if(canRechargeCamera) {
+                // amount of recharges 
+                charges++;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PhotoCapture.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime;
+
+    private Texture2D screenCapture;
+    private bool viewingPhoto;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Mouse1) && ItemCamera.canUseCamera) {
+            if(!viewingPhoto) {
+				StartCoroutine(CapturePhoto());
+			} else {
+                RemovePhoto();
+            }
+        }
+    }
+	private IEnumerator CameraFlashEffect() {
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
+	}
+
+	private IEnumerator CapturePhoto() {
+        // CameraUI set false
+        viewingPhoto = true;
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+
+    private void ShowPhoto() {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+		StartCoroutine(CameraFlashEffect());
+	}
+
+    private void RemovePhoto() {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
+    }
+}
