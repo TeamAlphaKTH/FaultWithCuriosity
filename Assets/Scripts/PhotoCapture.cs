ff263666a6d419ba834bfe3426739eac8907f80c
VicassoS <64210069+VicassoS@users.noreply.github.com>
2023-04-25 10:31:47 +0200

    move flash rourine

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -1,65 +1,66 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhotoCapture : MonoBehaviour
-{
-    [Header("Photo Taker")]
-    [SerializeField] public Image photoDisplayArea;
-    [SerializeField] public GameObject photoFrame;
+public class PhotoCapture:MonoBehaviour {
+	[Header("Photo Taker")]
+	[SerializeField] public Image photoDisplayArea;
+	[SerializeField] public GameObject photoFrame;
 
-    [Header("Flash Effect")]
-    [SerializeField] private GameObject cameraFlash;
-    [SerializeField] private float flashTime;
+	[Header("Flash Effect")]
+	[SerializeField] private GameObject cameraFlash;
+	[SerializeField] private float flashTime;
 
-    private Texture2D screenCapture;
-    private bool viewingPhoto;
+	private Texture2D screenCapture;
+	private bool viewingPhoto;
 
 	// Start is called before the first frame update
-	void Start()
-    {
-        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-    }
+	void Start() {
+		screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update() {
 		if(Input.GetKeyDown(KeyCode.Mouse1) && ItemCamera.canUseCamera) {
-            if(!viewingPhoto) {
+			if(!viewingPhoto) {
 				StartCoroutine(CapturePhoto());
 			} else {
-                RemovePhoto();
-            }
-        }
-    }
+				RemovePhoto();
+			}
+		}
+	}
+	/// <summary>
+	/// Increases light intensity on object - CameraFlash
+	/// </summary>
+	/// <returns></returns>
 	private IEnumerator CameraFlashEffect() {
-        cameraFlash.SetActive(true);
-        yield return new WaitForSeconds(flashTime);
-        cameraFlash.SetActive(false);
+		cameraFlash.SetActive(true);
+		yield return new WaitForSeconds(flashTime);
+		cameraFlash.SetActive(false);
 	}
 
 	private IEnumerator CapturePhoto() {
-        // CameraUI set false
-        viewingPhoto = true;
-        yield return new WaitForEndOfFrame();
+		// CameraUI set false
+		viewingPhoto = true;
+		yield return new WaitForEndOfFrame();
 
-        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
-        screenCapture.ReadPixels(regionToRead, 0, 0, false);
-        screenCapture.Apply();
-        //ShowPhoto();
-    }
+		Rect regionToRead = new(0, 0, Screen.width, Screen.height);
+		screenCapture.ReadPixels(regionToRead, 0, 0, false);
+		screenCapture.Apply();
 
-    public void ShowPhoto() {
-        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
-        photoDisplayArea.sprite = photoSprite;
-
-        photoFrame.SetActive(true);
 		StartCoroutine(CameraFlashEffect());
+		ShowPhoto();
 	}
 
-    private void RemovePhoto() {
-        viewingPhoto = false;
-        photoFrame.SetActive(false);
-    }
+	public void ShowPhoto() {
+		Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+		//photoDisplayArea.sprite = photoSprite;
+
+		photoFrame.SetActive(true);
+	}
+
+	private void RemovePhoto() {
+		viewingPhoto = false;
+		photoFrame.SetActive(false);
+	}
 }
