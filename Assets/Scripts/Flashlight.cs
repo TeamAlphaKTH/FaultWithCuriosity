3015ae29870694e938aadb83cf932a38f610db2f
Serkan Anar <114192161+sxkho@users.noreply.github.com>
2023-04-25 11:08:35 +0200

    Merge branch paranoia to dev (#34)
    * Stamina UI update
    
    * Add paranoia and UI
    
    Co-Authored-By: akessonalfred <102586522+akessonalfred@users.noreply.github.com>
    
    * Add UI, add paranoia, fix flicker.
    
    Co-Authored-By: akessonalfred <102586522+akessonalfred@users.noreply.github.com>
    
    * Fix UI, fix code.
    
    Co-Authored-By: akessonalfred <102586522+akessonalfred@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kenan Dizdarevic <kenandi@kth.se>
    Co-authored-by: akessonalfred <102586522+akessonalfred@users.noreply.github.com>

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Flashlight:MonoBehaviour {
     [Header("Flashlight Parameters")]
@@ -9,23 +10,34 @@ public class Flashlight:MonoBehaviour {
     [SerializeField] private float batterySpeed = 10f;
     [SerializeField] private float incrementBattery = 15f;
     [SerializeField] private KeyCode rechargeBattery = KeyCode.B;
+    [SerializeField] private Image batteryBlock1;
+    [SerializeField] private Image batteryBlock2;
+    [SerializeField] private Image batteryBlock3;
 
     private bool flashlightActive = false;
     private bool canUseFlashlight = true;
     public float batteryLevel = 100;
     private float minIntensity = 1f;
     private float maxIntensity = 7f;
-    private float flickerDuration = 0.2f;
+    private float flickerDuration = 0.65f;
     private float flickerDelay = 0.1f;
-    private bool isFlickering = false;
+    [SerializeField] private bool isFlickering = false;
+
+    [Header("Paranoia parameters")]
+    [SerializeField] private float maxParanoia = 100f;
+    [SerializeField] private float paranoiaIncrements = 0.2f;
+    [SerializeField] private float currentParanoia;
+    [SerializeField] private Slider paranoiaSlider;
 
     private void Awake() {
         batteryText.SetText("100%");
+        currentParanoia = 0;
     }
 
     void Start() {
         // Flashlight starts off
         FlashlightLight.gameObject.SetActive(false);
+        paranoiaSlider.value = 0f;
     }
 
     void Update() {
@@ -46,13 +58,41 @@ public class Flashlight:MonoBehaviour {
             if(batteryLevel > 100) {
                 batteryLevel = 100;
             }
+
+            if(!isFlickering && batteryLevel > 33) {
+                CancelInvoke("Flicker");
+                FlashlightLight.intensity = maxIntensity;
+            }
+        }
+        if(batteryLevel <= 100 && batteryLevel > 50) {
+            batteryBlock3.enabled = true;
+            batteryBlock2.enabled = true;
+            batteryBlock1.enabled = true;
+            batteryBlock3.color = new Color32(0, 255, 19, 120);
+            batteryBlock2.color = new Color32(0, 255, 19, 120);
+            batteryBlock1.color = new Color32(0, 255, 19, 120);
+
+
+        } else if(batteryLevel <= 50 && batteryLevel >= 10) {
+            batteryBlock3.enabled = false;
+            batteryBlock2.enabled = true;
+            batteryBlock1.enabled = true;
+            batteryBlock2.color = new Color32(255, 241, 0, 120);
+            batteryBlock1.color = new Color32(255, 241, 0, 120);
+
+        } else if(batteryLevel < 10 && batteryLevel > 0) {
+            batteryBlock2.enabled = false;
+            batteryBlock1.enabled = true;
+            batteryBlock1.color = new Color32(255, 32, 0, 120);
+        } else if(batteryLevel <= 0) {
+            batteryBlock1.enabled = false;
         }
 
         // Handle battery level and flickering
-        if(flashlightActive && batteryLevel > 0) {
+        if(flashlightActive && batteryLevel >= 0) {
             batteryLevel -= batterySpeed * Time.deltaTime;
 
-            if(batteryLevel < 20) {
+            if(batteryLevel < 10) {
                 if(!isFlickering) {
                     isFlickering = true;
                     InvokeRepeating("Flicker", flickerDelay, flickerDuration);
@@ -69,9 +109,18 @@ public class Flashlight:MonoBehaviour {
             FlashlightLight.gameObject.SetActive(false);
             flashlightActive = false;
         }
-        batteryText.SetText(batteryLevel.ToString("F0") + "%");
-    }
 
+        if(!flashlightActive && currentParanoia < 100) {
+            currentParanoia = currentParanoia * 1.001f + paranoiaIncrements * Time.deltaTime;
+        }
+
+        if(currentParanoia >= 100) {
+            Debug.Log("Dead");
+        }
+
+        batteryText.SetText(batteryLevel.ToString("F0"));
+        paranoiaSlider.value = currentParanoia;
+    }
 
     private void Flicker() {
         float randomIntensity = Random.Range(minIntensity, maxIntensity);
