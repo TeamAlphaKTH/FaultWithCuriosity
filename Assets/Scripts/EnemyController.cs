850684c87f7aea11f6bed7c86ccb5a3a1785f97d
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-04-26 15:50:27 +0200

    Add enemy dealing damage to close player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController:MonoBehaviour {
+	[SerializeField] private float damageMultiplier = 0.1f;
+	[SerializeField] private float damageDistance = 3f;
+
 	private NavMeshAgent enemyAIAgent;
 	private GameObject player;
+	private float distanceToPlayer;
+	private float currentParanoia;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -13,13 +18,28 @@ public class EnemyController:MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
+		currentParanoia = Flashlight.currentParanoia;
+
 		//Get the current paranoia 10s for distance
-		int paranoiaDistance = 102 - (int)Flashlight.currentParanoia * 10 / 10;
+		int paranoiaDistance = 105 - (int)currentParanoia * 10 / 10;
+
+		distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
 		//If the player is further than the paranoiaDistance, move the AI closer.
-		if(Vector3.Distance(player.transform.position, transform.position) > paranoiaDistance) {
+		if(distanceToPlayer > paranoiaDistance) {
 			enemyAIAgent.destination = player.transform.position;
 			enemyAIAgent.stoppingDistance = paranoiaDistance;
+		}
+
+		DealDamage();
+	}
+
+	/// <summary>
+	/// Increase the paranoia level of the <see cref="player"/>.
+	/// </summary>
+	private void DealDamage() {
+		if(distanceToPlayer < damageDistance && currentParanoia < 100) {
+			Flashlight.currentParanoia += (damageDistance - distanceToPlayer) * damageMultiplier;
 		}
 	}
 }
