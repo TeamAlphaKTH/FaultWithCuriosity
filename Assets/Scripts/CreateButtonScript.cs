2cea508a8afd781e10dbe8680551609f918c0da6
MT0DE <lukas.lannge@hotmail.se>
2023-04-26 08:46:46 +0200

    Add create-button logic to show IP during hosting
    Use same logic for pause menu

---
+++ Assets/Scripts/CreateButtonScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class JoinButtonScript {
+	public void OnButtonPress() {
+		//find local ips using the [port]
+		string localIp = GetLocalIpadress();
+		ushort port = 7777;
+		NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port);
+
+		//Needs to have Netcode downloaded
+		//start hosting, show text during COOP option and in pasue menu
+		//NetworkManager.Singleton.StartHost();
+	}
+
+	public static string GetLocalIpadress() {
+		IPHostEntry ips = Dns.GetHostEntry(Dns.GetHostName());
+		foreach (var ip in ips.AddressList) {
+			if (ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
+				return ip.ToString();
+		}
+		throw new Exception("Could not find current (if any) connected IPv4 address");
+	}
+}
