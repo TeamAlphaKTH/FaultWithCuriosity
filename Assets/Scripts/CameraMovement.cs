82b0a73f19f48177e97b9fa925291ebcb338a129
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-04-26 10:24:08 +0200

    Add Inventory and Interactions (#41)
    * Move flashlight images to texture folder
    
    * Add inventory icon textures
    
    * Fix inventory icons to sprites
    
    * Add Inventory.cs
    
    * Update Testing.unity
    
    * Update Testing.unity
    
    * Add pixel texture
    
    * Make Flashlight batterylevel static
    
    * Add Inventory and script
    
    * Add Battery model
    
    * Add prefab for inventory and battery
    
    * Add pills prefab
    
    * Add pills model
    
    * Update battery and pill prefab
    
    * Update InventoryUI Prefab
    
    * Update Testing.unity
    
    * Update Inventory
    
    * Add Interactable interface
    
    * Add scripts for Item pill, door and battery
    
    * Add ItemUI Prefab
    
    * Delete DestroyOnInteract Script
    
    Not used
    
    * Update prefabs
    
    * Update Scripts
    
    * Move Script
    
    * Update Script and put in UI to Dungeon map
    
    * Add UI prefab
    
    All UIs in one prefab
    
    * Update Prefab and Dungeon
    
    * Bugfix Sprint
    
    ---------
    
    Co-authored-by: Hugo <54435874+Cactooz@users.noreply.github.com>
    Co-authored-by: Kenan Dizdarevic <kenandi@kth.se>

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 
 public class CameraMovement:MonoBehaviour {
+    public static bool CanRotate { get; set; } = true;
     private float xRotation;
     private float yRotation;
     [SerializeField] private float sensitivity; //Make global for settings later
     [SerializeField] private Transform person;
+
+    // Interaction variables
+    private Camera mainCamera;
+    [SerializeField] private float interactionRange = 5f;
+    private IInteractable currentTarget;
+
+    [SerializeField] public static KeyCode interactKey = KeyCode.E;
+
+
+    private void Awake() {
+        mainCamera = Camera.main;
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -13,14 +26,78 @@ public class CameraMovement:MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //x-axis controls up and down rotation, y-axis controls left and right rotation.
-        yRotation += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
-        xRotation -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        if(CanRotate) {
+            //x-axis controls up and down rotation, y-axis controls left and right rotation.
+            yRotation += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+            xRotation -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
 
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
 
-        //Rotates the body sideways.
-        person.rotation = Quaternion.Euler(0, yRotation, 0);
+            //Rotates the body sideways.
+            person.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+            RaycastForInteractable();
+            if(Input.GetKeyDown(interactKey)) {
+                if(currentTarget != null) {
+                    currentTarget.OnInteract();
+                }
+            }
+        }
+    }
+
+
+    private void RaycastForInteractable() {
+        RaycastHit hitTarget;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hitTarget, interactionRange)) {
+            IInteractable interactable = hitTarget.collider.GetComponent<IInteractable>();
+
+            if(interactable != null) {
+                if(hitTarget.distance <= interactable.MaxRange) {
+                    // We are looking at an interactable within range.
+                    if(interactable == currentTarget) {
+                        // We are already looking at this interactable.
+                        return;
+                    } else if(currentTarget != null) {
+                        // We are looking at an interactable.
+                        currentTarget.OnEndHover();
+                        currentTarget = interactable;
+                        currentTarget.OnStartHover();
+                        return;
+                    } else {
+                        // We are looking at an interactable for the first time.
+                        currentTarget = interactable;
+                        currentTarget.OnStartHover();
+                        return;
+                    }
+                } else {
+                    if(currentTarget != null) {
+                        // We are looking at an interactable, but it is out of range.
+                        currentTarget.OnEndHover();
+                        currentTarget = null;
+                        return;
+                    }
+                }
+            } else {
+                // We are looking at something that is not an interactable.
+                if(currentTarget != null) {
+                    currentTarget.OnEndHover();
+                    currentTarget = null;
+                    return;
+                }
+            }
+        } else {
+            // We are looking at nothing.
+            if(currentTarget != null) {
+                currentTarget.OnEndHover();
+                currentTarget = null;
+                return;
+            }
+        }
     }
 }
+
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,90 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory:MonoBehaviour {
+	[SerializeField] private KeyCode openInventory = KeyCode.Tab;
+	[SerializeField] private GameObject inventory;
+	[SerializeField] private TextMeshProUGUI drugText;
+	public static int drugNr;
+	[SerializeField] private GameObject battery;
+	[SerializeField] private GameObject pills;
+
+	[SerializeField] private TextMeshProUGUI batteryText;
+	public static int batteryNr;
+	[SerializeField] private Slider flashlightSlider;
+	[SerializeField] private Slider cameraSlider;
+
+	private void Start() {
+		flashlightSlider.value = Flashlight.batteryLevel;
+		Debug.Log("isStart");
+	}
+
+	// Update is called once per frame
+	void Update() {
+		drugText.text = drugNr.ToString();
+		batteryText.text = batteryNr.ToString();
+		if(Input.GetKeyDown(openInventory)) {
+			switch(inventory.activeSelf) {
+				case true:
+				inventory.SetActive(false);
+				Cursor.lockState = CursorLockMode.Locked;
+				// FirstPersonController.CanMove = true;
+				CameraMovement.CanRotate = true;
+
+				break;
+				case false:
+				inventory.SetActive(true);
+				Cursor.lockState = CursorLockMode.Confined;
+				// FirstPersonController.CanMove = false;
+				CameraMovement.CanRotate = false;
+				break;
+			}
+		}
+
+		flashlightSlider.value = Flashlight.batteryLevel;
+	}
+
+	public void UseDrugs() {
+		drugNr = int.Parse(drugText.text);
+		if(drugNr > 0) {
+			drugNr--;
+			Flashlight.currentParanoia = Flashlight.currentParanoia <= 20 ? 0 : Flashlight.currentParanoia -= 20;
+		}
+	}
+
+	public void DropDrugs() {
+		drugNr = int.Parse(drugText.text);
+		if(drugNr > 0) {
+			drugNr--;
+			Instantiate(pills, FirstPersonController.characterController.transform.position + new Vector3(0, 1, 0.2f), Quaternion.identity);
+		}
+	}
+
+	public void RechargeCamera() {
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0 && cameraSlider.value < 3) {
+			batteryNr--;
+			cameraSlider.value++;
+		}
+
+	}
+	public void RechargeFlashlight() {
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0 && Flashlight.batteryLevel != 100) {
+			batteryNr--;
+			Flashlight.batteryLevel = Flashlight.batteryLevel >= 80 ? 100 : Flashlight.batteryLevel += 20;
+		}
+
+	}
+	public void DropBattery() {
+		Debug.Log("isrunning");
+		batteryNr = int.Parse(batteryText.text);
+		if(batteryNr > 0) {
+			batteryNr--;
+			Instantiate(battery, FirstPersonController.characterController.transform.position + new Vector3(0, 1, 0.2f), Quaternion.identity);
+
+		}
+	}
+
+}
