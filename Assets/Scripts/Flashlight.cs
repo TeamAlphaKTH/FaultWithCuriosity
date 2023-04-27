8100f872e9c2a8074805226b45cfe5aeee53593b
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-04-27 16:45:21 +0200

    Update stuff

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -67,60 +67,56 @@ public class Flashlight:NetworkBehaviour {
 
 	private void FlashlightControl() {
 		// Toggle flashlight on/off with key press
-		if(Input.GetKeyDown(flashlightKey)) {
-			if(flashlightActive) {
-				ChangeLightIntensityServerRpc(0);
-				flashlightActive = false;
-			} else {
-				ChangeLightIntensityServerRpc(maxIntensity);
-				flashlightActive = true;
+		if(!PauseMenu.paused)
+			if(Input.GetKeyDown(flashlightKey)) {
+				if(flashlightActive) {
+					ChangeLightIntensityServerRpc(0);
+					flashlightActive = false;
+				} else {
+					ChangeLightIntensityServerRpc(maxIntensity);
+					flashlightActive = true;
+				}
 			}
-		}
 
-		if (!PauseMenu.paused) {
-			// Toggle flashlight on/off with key press
-			if (Input.GetKeyDown(flashlightKey)) {
-				flashlightActive = !flashlightActive;
-				flashlightLight.gameObject.SetActive(flashlightActive);
-			}
+		{
 
 			if(!isFlickering && batteryLevel > 10) {
 				CancelInvoke("Flicker");
 				ChangeLightIntensityServerRpc(maxIntensity);
 			}
 		}
-		if (batteryLevel <= 100 && batteryLevel > 50) {
+		if(batteryLevel <= 100 && batteryLevel > 50) {
 			batteryBlock3.enabled = true;
 			batteryBlock2.enabled = true;
 			batteryBlock1.enabled = true;
 			batteryBlock3.color = new Color32(0, 255, 19, 120);
 			batteryBlock2.color = new Color32(0, 255, 19, 120);
 			batteryBlock1.color = new Color32(0, 255, 19, 120);
-		} else if (batteryLevel <= 50 && batteryLevel >= 10) {
+		} else if(batteryLevel <= 50 && batteryLevel >= 10) {
 			batteryBlock3.enabled = false;
 			batteryBlock2.enabled = true;
 			batteryBlock1.enabled = true;
 			batteryBlock2.color = new Color32(255, 241, 0, 120);
 			batteryBlock1.color = new Color32(255, 241, 0, 120);
-		} else if (batteryLevel < 10 && batteryLevel > 0) {
+		} else if(batteryLevel < 10 && batteryLevel > 0) {
 			batteryBlock2.enabled = false;
 			batteryBlock1.enabled = true;
 			batteryBlock1.color = new Color32(255, 32, 0, 120);
-		} else if (batteryLevel <= 0) {
+		} else if(batteryLevel <= 0) {
 			batteryBlock1.enabled = false;
 		}
 
 		// Handle battery level and flickering
-		if (flashlightActive && batteryLevel >= 0) {
+		if(flashlightActive && batteryLevel >= 0) {
 			batteryLevel -= batterySpeed * Time.deltaTime;
 
-			if (batteryLevel < 10) {
-				if (!isFlickering) {
+			if(batteryLevel < 10) {
+				if(!isFlickering) {
 					isFlickering = true;
 					InvokeRepeating("Flicker", flickerDelay, flickerDuration);
 				}
 			} else {
-				if (isFlickering) {
+				if(isFlickering) {
 					isFlickering = false;
 					CancelInvoke("Flicker");
 				}
@@ -132,11 +128,11 @@ public class Flashlight:NetworkBehaviour {
 			flashlightActive = false;
 		}
 
-		if (!flashlightActive && currentParanoia < 100) {
+		if(!flashlightActive && currentParanoia < 100) {
 			currentParanoia += paranoiaIncrements * Time.deltaTime;
 		}
 
-		if (currentParanoia >= 100) {
+		if(currentParanoia >= 100) {
 			Debug.Log("Dead");
 		}
 
