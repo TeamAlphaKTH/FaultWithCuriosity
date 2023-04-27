e0994e31ea37ae81a8271ccb5fb3afc7bdd51271
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-04-27 15:00:56 +0200

    Add an enemy AI (#48)
    * Install AI package
    
    * Remove movement speed debug logging
    
    * Add AI pathfinding dependent on paranoia level
    
    * Add enemy dealing damage to close player
    
    * Add Enemy prefab
    
    * Use SetDestination() instead of .destination
    
    * Update enemyController to get player transform
    
    * Add script to bake the full NavMeshArea on start
    
    * Add dungeon to the testing scene
    
    * Make dungeon model readable
    
    * Fix warnings in test scene
    
    * Fix how close the ghost is to the player
    
    * Make enemy move to new destination earlier

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController:MonoBehaviour {
+	[SerializeField] private float damageMultiplier = 0.1f;
+	[SerializeField] private float damageDistance = 3f;
+
+	private NavMeshAgent enemyAIAgent;
+	private Transform player;
+	private float distanceToPlayer;
+	private float currentParanoia;
+
+	// Start is called before the first frame update
+	void Start() {
+		player = GameObject.Find("Player Aaron").GetComponent<Transform>();
+		enemyAIAgent = gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		currentParanoia = Flashlight.currentParanoia;
+
+		//Get the current paranoia 10s for distance
+		int paranoiaDistance = 101 - (int)Math.Floor(currentParanoia);
+
+		distanceToPlayer = Vector3.Distance(player.position, transform.position);
+
+		//If the player is further than the paranoiaDistance, move the AI closer.
+		if(distanceToPlayer * 3 > paranoiaDistance) {
+			enemyAIAgent.SetDestination(player.position);
+			enemyAIAgent.stoppingDistance = paranoiaDistance;
+		} else {
+			DealDamage();
+		}
+	}
+
+	/// <summary>
+	/// Increase the paranoia level of the <see cref="player"/>.
+	/// </summary>
+	private void DealDamage() {
+		if(distanceToPlayer < damageDistance && currentParanoia < 100) {
+			Flashlight.currentParanoia += (damageDistance - distanceToPlayer) * damageMultiplier;
+		}
+	}
+}
