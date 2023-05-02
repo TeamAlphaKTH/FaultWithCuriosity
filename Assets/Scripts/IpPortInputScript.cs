2b1407d10eaec4b0acbc0123dd4c5850417a0733
sxkho <Serkan.anar24@gmail.com>
2023-05-02 09:24:47 +0200

    Add loadScene

--- Assets/Scripts/IpPortInputScript.cs
+++ Assets/Scripts/IpPortInputScript.cs
@@ -2,33 +2,35 @@ using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class IpPortInputScript : NetworkBehaviour {
-	[SerializeField] private TMP_InputField ipAndPort;
-	[SerializeField] private Button joinButton;
-	string inputText;
+public class IpPortInputScript:NetworkBehaviour {
+    [SerializeField] private TMP_InputField ipAndPort;
+    [SerializeField] private Button joinButton;
+    string inputText;
 
-	public ushort portNumber;
-	public string ipAddress;
+    public ushort portNumber;
+    public string ipAddress;
 
-	void Start() {
-		joinButton.onClick.AddListener(OnSubmitInfo);
-	}
+    void Start() {
+        joinButton.onClick.AddListener(OnSubmitInfo);
+    }
 
-	private void OnSubmitInfo() {
-		inputText = ipAndPort.text;
-		string[] stringParts = inputText.Split(":");
-		if (stringParts.Length == 2) {
-			ipAddress = stringParts[0].Trim();
-			ushort.TryParse(stringParts[1].Trim(), out portNumber);
-			if (portNumber > 65535) {
-				portNumber = 7777;
-			} else if (portNumber < 0) {
-				portNumber = 7777;
-			}
-			NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipAddress, portNumber);
-			NetworkManager.Singleton.StartClient();
-		}
-	}
+    private void OnSubmitInfo() {
+        inputText = ipAndPort.text;
+        string[] stringParts = inputText.Split(":");
+        if(stringParts.Length == 2) {
+            ipAddress = stringParts[0].Trim();
+            ushort.TryParse(stringParts[1].Trim(), out portNumber);
+            if(portNumber > 65535) {
+                portNumber = 7777;
+            } else if(portNumber < 0) {
+                portNumber = 7777;
+            }
+            SceneManager.LoadScene("Dungeon");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipAddress, portNumber);
+            NetworkManager.Singleton.StartClient();
+        }
+    }
 }
