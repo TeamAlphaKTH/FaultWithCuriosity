4daac363c2769c9674bcf5d3680a2d865ab63af9
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-05-02 13:07:15 +0200

    Add lock check to Door script

--- Assets/Scripts/Item Scripts/Door.cs
+++ Assets/Scripts/Item Scripts/Door.cs
@@ -2,16 +2,28 @@ using TMPro;
 using UnityEngine;
 
 public class Door:MonoBehaviour, IInteractable {
+	[SerializeField] private bool locked = false;
+	[SerializeField] private int keyId;
+
 	private TextMeshProUGUI itemText;
 	private GameObject itemUI;
 	private Animator animator;
 	public float MaxRange { get { return maxRange; } }
 	private const float maxRange = 100f;
+
 	public void OnEndHover() {
 		itemText.text = "";
 	}
 
 	public void OnInteract() {
+		if(locked) {
+			if(Inventory.keyIds.Contains(keyId)) {
+				locked = false;
+				OnStartHover();
+			}
+			return;
+		}
+
 		if(!animator.GetBool("OpenDoor")) {
 			animator.SetBool("OpenDoor", true);
 			animator.SetBool("CloseDoor", false);
@@ -22,7 +34,15 @@ public class Door:MonoBehaviour, IInteractable {
 	}
 
 	public void OnStartHover() {
-		itemText.text = "Press " + CameraMovement.interactKey + " to use door";
+		if(locked) {
+			if(Inventory.keyIds.Contains(keyId)) {
+				itemText.text = "Press " + CameraMovement.interactKey + " to unlock the door";
+			} else {
+				itemText.text = "The door is locked";
+			}
+		} else {
+			itemText.text = "Press " + CameraMovement.interactKey + " to use door";
+		}
 	}
 	private void Start() {
 		itemUI = GameObject.Find("ItemUI");
