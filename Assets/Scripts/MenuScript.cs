abc77cf4070c76a5a44d7983e9085b9eff9387f7
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-05-02 08:57:27 +0200

    Add Co-op (#57)
    * Add NetworkManager
    
    * Add ClientNetworkTransform Script
    
    So client can send position to host
    
    * Update Prefabs
    
    * Add ParrelSync
    
    * Update FirstPersonController.cs
    
    * Add textmeshpro and interactive HUD
    
    * Update testing.unity
    
    * Fixes
    
    * Fixed flashlight bug
    
    * Update stuff
    
    * Update Door Prefab
    
    * Update Dungeon with network manager
    
    ---------
    
    Co-authored-by: MT0DE <lukas.lannge@hotmail.se>

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : NetworkBehaviour {
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button connectButton;
+
+    private void Awake() {
+        hostButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        connectButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+
+    private void Update() {
+
+    }
+}
