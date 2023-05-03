0a94ccdce18a4120baadad2f152686c874f2967a
Hugo <54435874+Cactooz@users.noreply.github.com>
2023-05-03 15:57:25 +0200

    Add option if the input should be on or off

--- Assets/Scripts/PuzzleInputController.cs
+++ Assets/Scripts/PuzzleInputController.cs
@@ -1,8 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+internal class Inputs {
+	public GameObject inputObject;
+	public bool enabled;
+}
+
 public class PuzzleInputController:MonoBehaviour {
-	[SerializeField] private List<GameObject> buttons = new();
+	[SerializeField] private List<Inputs> inputs = new();
 	[SerializeField] private GameObject door;
 
 	// Start is called before the first frame update
@@ -12,12 +18,18 @@ public class PuzzleInputController:MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		foreach(GameObject button in buttons) {
-			if(!button.GetComponentInChildren<ButtonController>().clicked) {
+		foreach(Inputs input in inputs) {
+			//Get if the input is currently clicked/enabled
+			bool clicked = input.inputObject.GetComponentInChildren<ButtonController>().clicked;
+			//Get if that input object should be clicked/enabled
+			bool enabled = input.enabled;
+			//Stop looping through if one is false
+			if((!clicked && enabled) || (clicked && !enabled)) {
 				return;
 			}
 		}
 
+		//Unlock the defined door if everything is correct
 		door.GetComponent<Door>().locked = false;
 	}
 }
