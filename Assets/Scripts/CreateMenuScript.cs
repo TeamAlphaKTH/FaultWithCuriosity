cc5c2713829e1d608ed15e8357dcfb71ebea8faa
Serkan Anar <114192161+sxkho@users.noreply.github.com>
2023-05-03 13:22:49 +0200

    Add single player menu logic. (#68)
    * Fix singleplayer.
    
    Both singleplayer and multiplayer work now.
    
    * Update MainMenu
    
    * Update files with new formatting according to Guidlines
    
    ---------
    
    Co-authored-by: MT0DE <lukas.lannge@hotmail.se>

--- Assets/Scripts/CreateMenuScript.cs
+++ Assets/Scripts/CreateMenuScript.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class CreateButtonScript : NetworkBehaviour {
+public class CreateButtonScript:NetworkBehaviour {
 
 	private TMP_InputField portnumberInput;
 	private Button btn;
@@ -22,44 +22,44 @@ public class CreateButtonScript : NetworkBehaviour {
 		btn.onClick.AddListener(() => {
 			OnButtonPress();
 		});
-		SceneManager.sceneLoaded += SceneManager_sceneLoaded;
 	}
 	public void OnButtonPress() {
-		//find local ips using the [port]
+		//Find local ips using the [port]
 		localIp = GetLocalIpadress();
 		port = 0;
 
 		//Parse the port text into a ushort 
 		try {
 			port = ushort.Parse(portnumberInput.text);
-			if (port < 0 || port > 65535) {
+			if(port < 0 || port > 65535) {
 				port = 7777;
 				portnumberInput.text = localIp + ":" + port;
 			}
-		} catch (FormatException) {
+		} catch(FormatException) {
 			Debug.Log("Portnumber cannot contain any letters");
 			return;
 		}
 		portnumberInput.text = localIp + ":" + port;
 
 		//Logic for when port is correct
-		if (!wrongFormat) {
+		if(!wrongFormat) {
 			//Load the scene, the EventHandler will start hosting
+			SceneManager.sceneLoaded += SceneManager_sceneLoaded;
 			SceneManager.LoadScene("Dungeon");
 		}
 		wrongFormat = false;
 	}
 	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
-		if (arg0.name.Equals("Dungeon")) {
+		if(arg0.name.Equals("Dungeon")) {
+			SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
 			NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port, localIp);
 			NetworkManager.Singleton.StartHost();
 		}
 	}
-
 	public static string GetLocalIpadress() {
 		IPHostEntry ips = Dns.GetHostEntry(Dns.GetHostName());
-		foreach (var ip in ips.AddressList) {
-			if (ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
+		foreach(var ip in ips.AddressList) {
+			if(ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
 				return ip.ToString();
 		}
 		throw new Exception("Could not find current (if any) connected IPv4 address");
