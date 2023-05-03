647c318ee1aa6a7b897f306fd31c3dfecb61e33f
sxkho <Serkan.anar24@gmail.com>
2023-05-03 08:21:01 +0200

    Fix Main Menu issue
    Joining each other works

--- Assets/Scripts/CreateMenuScript.cs
+++ Assets/Scripts/CreateMenuScript.cs
@@ -8,69 +8,52 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class CreateButtonScript : NetworkBehaviour {
+public class CreateButtonScript:NetworkBehaviour {
 
-	private TMP_InputField portnumberInput;
-	private Button btn;
+    [SerializeField] private TMP_InputField portnumberInput;
+    [SerializeField] private Button btn;
+    private bool wrongFormat = false;
+    private string localIp;
+    private ushort port;
 
-	private bool wrongFormat = false;
-	private string localIp;
-	private ushort port;
+    private void Start() {
+        btn.onClick.AddListener(() => {
+            OnButtonPress();
+        });
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+    public void OnButtonPress() {
+        //find local ips using the [port]
+        localIp = GetLocalIpadress();
+        port = 0;
 
-	private void Start() {
-		portnumberInput = GameObject.Find("/Main Menu/CreateGameMenu").GetComponentInChildren<TMP_InputField>();
-		btn = GameObject.Find("/Main Menu/CreateGameMenu").GetComponentInChildren<Button>();
+        //Parse the port text into a ushort 
+        try {
+            port = ushort.Parse(portnumberInput.text);
+        } catch(FormatException) {
+            Debug.Log("Portnumber cannot contain any letters");
+            return;
+        }
+        portnumberInput.text = localIp + ":" + port;
 
-		btn.onClick.AddListener(() => {
-			OnButtonPress();
-		});
-		SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-	}
-	public void OnButtonPress() {
-		//find local ips using the [port]
-		localIp = GetLocalIpadress();
-		port = 0;
+        //Logic for when port is correct
+        if(!wrongFormat) {
+            //Load the scene, the EventHandler will start hosting
+            SceneManager.LoadScene("Dungeon");
+        }
+        wrongFormat = false;
+    }
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port, localIp);
+        NetworkManager.Singleton.StartHost();
+    }
 
-		//Parse the port text into a ushort 
-		try {
-			port = ushort.Parse(portnumberInput.text);
-			if (port < 0 || port > 65535) {
-				port = 7777;
-				portnumberInput.text = localIp + ":" + port;
-			}
-		} catch (FormatException) {
-			Debug.Log("Portnumber cannot contain any letters");
-			return;
-		}
-
-		//Logic for when port is correct
-		if (!wrongFormat) {
-			//Load the scene, the EventHandler will start hosting
-			SceneManager.LoadScene("Dungeon");
-		}
-		wrongFormat = false;
-	}
-	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
-		NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port);
-		NetworkManager.Singleton.StartHost();
-		//if (arg0.isLoaded && arg0.name.Equals("Dungeon")) {
-		//	NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port);
-		//	NetworkManager.Singleton.StartHost();
-		//}
-		//To Return the player to the initial start menu
-		//else if(arg0.name.Equals("MainMenu")) {
-		//	GameObject.Find("CreateGameMenu").SetActive(false);
-		//	GameObject.Find("Join Game Menu").SetActive(false);
-		//	GameObject.Find("Main Menu").SetActive(true);
-		//}
-	}
-
-	public static string GetLocalIpadress() {
-		IPHostEntry ips = Dns.GetHostEntry(Dns.GetHostName());
-		foreach (var ip in ips.AddressList) {
-			if (ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
-				return ip.ToString();
-		}
-		throw new Exception("Could not find current (if any) connected IPv4 address");
-	}
+    public static string GetLocalIpadress() {
+        IPHostEntry ips = Dns.GetHostEntry(Dns.GetHostName());
+        foreach(var ip in ips.AddressList) {
+            if(ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
+                return ip.ToString();
+        }
+        throw new Exception("Could not find current (if any) connected IPv4 address");
+    }
 }
--- Assets/Scripts/IpPortInputScript.cs
+++ Assets/Scripts/IpPortInputScript.cs
@@ -5,43 +5,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class IpPortInputScript : NetworkBehaviour {
-	private TMP_InputField ipAndPort;
-	private Button joinButton;
+public class IpPortInputScript:NetworkBehaviour {
+    [SerializeField] private TMP_InputField ipAndPort;
+    [SerializeField] private Button joinButton;
+    string inputText;
 
-	private string inputText;
-	private ushort portNumber;
-	private string ipAddress;
+    public ushort portNumber;
+    public string ipAddress;
 
-	void Start() {
-		ipAndPort = GameObject.Find("/Main Menu/Join Game Menu").GetComponentInChildren<TMP_InputField>();
-		joinButton = GameObject.Find("/Main Menu/Join Game Menu").GetComponentInChildren<Button>();
+    void Start() {
+        joinButton.onClick.AddListener(OnSubmitInfo);
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
 
-		joinButton.onClick.AddListener(OnSubmitInfo);
-		SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-	}
+    private void OnSubmitInfo() {
+        inputText = ipAndPort.text;
+        string[] stringParts = inputText.Split(":");
+        if(stringParts.Length == 2) {
+            ipAddress = stringParts[0].Trim();
+            ushort.TryParse(stringParts[1].Trim(), out portNumber);
+            if(portNumber > 65535) {
+                portNumber = 7777;
+            } else if(portNumber < 0) {
+                portNumber = 7777;
+            }
+            SceneManager.LoadScene("Dungeon");
+        }
+    }
 
-	private void OnSubmitInfo() {
-		inputText = ipAndPort.text;
-		string[] stringParts = inputText.Split(":");
-		if (stringParts.Length == 2) {
-			ipAddress = stringParts[0].Trim();
-			ushort.TryParse(stringParts[1].Trim(), out portNumber);
-			if (portNumber > 65535) {
-				portNumber = 7777;
-			} else if (portNumber < 0) {
-				portNumber = 7777;
-			}
-			SceneManager.LoadScene("Dungeon");
-		} else if (stringParts.Length == 1 && stringParts[0].Contains(".")) {
-			ipAddress = stringParts[0].Trim();
-			portNumber = 7777;
-			SceneManager.LoadScene("Dungeon");
-		}
-	}
-
-	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
-		NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipAddress, portNumber);
-		NetworkManager.Singleton.StartClient();
-	}
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ipAddress, portNumber);
+        NetworkManager.Singleton.StartClient();
+    }
 }
