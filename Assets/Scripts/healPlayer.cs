463132a6d9621bd78814bb46d437ea20459852db
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-05-08 16:22:17 +0200

    Update all

--- Assets/Scripts/healPlayer.cs
+++ Assets/Scripts/healPlayer.cs
@@ -30,10 +30,8 @@ public class healPlayer:MonoBehaviour, IInteractable {
 	public void OnStartHover() {
 		if(user.isDead && Inventory.drugNr > 0) {
 			itemText.text = "Press " + CameraMovement.interactKey + " to revive";
-			OnStartHover();
 		} else if(user.isDead) {
 			itemText.text = "Get a pill to revive";
-			OnStartHover();
 		}
 	}
 
