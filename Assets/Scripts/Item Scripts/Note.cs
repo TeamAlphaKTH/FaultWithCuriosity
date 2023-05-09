b86927c9b06813002726c721c8f8be0fe8c6e141
Kenan Dizdarevic <kenandi@kth.se>
2023-05-09 08:12:47 +0200

    Fix note bug

--- Assets/Scripts/Item Scripts/Note.cs
+++ Assets/Scripts/Item Scripts/Note.cs
@@ -13,6 +13,9 @@ public class Note:MonoBehaviour, IInteractable {
 	public Door door;
 	public keypadScript keypadScript;
 
+	// Change this to false if you want to use a note without a code lock.
+	[SerializeField] private bool isCodeLock = true;
+
 	public void OnEndHover() {
 		itemText.text = "";
 	}
@@ -20,7 +23,7 @@ public class Note:MonoBehaviour, IInteractable {
 	public void OnInteract() {
 		itemText.text = "";
 		// Set the code text to the code of the door
-		if(transform.parent.parent.CompareTag("CodeLock")) {
+		if(isCodeLock) {
 			codeText.text = "CODE: \n " + door.code.Value;
 		} else {
 			codeText.text = noteUI.GetComponentInChildren<TMP_Text>().text;
@@ -42,7 +45,7 @@ public class Note:MonoBehaviour, IInteractable {
 	// Start is called before the first frame update
 	void Start() {
 		// Get door component.
-		if(transform.parent.parent.CompareTag("CodeLock")) {
+		if(isCodeLock) {
 			door = transform.parent.parent.GetChild(1).GetChild(0).GetComponent<Door>();
 		}
 
