3139964d5326a7e448fa5bb6f4f8c5fdcdaca7cc
sxkho <Serkan.anar24@gmail.com>
2023-05-09 16:18:46 +0200

    Fix go to menu instead of exit

--- Assets/Scripts/uiManager.cs
+++ Assets/Scripts/uiManager.cs
@@ -1,11 +1,15 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class uiManager:NetworkBehaviour {
+public class uiManager : NetworkBehaviour
+{
 	// Update is called once per frame
-	void Update() {
-		if(Input.GetKeyDown(KeyCode.Q)) {
-			Application.Quit();
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			SceneManager.LoadScene(0);
 		}
 	}
 }
