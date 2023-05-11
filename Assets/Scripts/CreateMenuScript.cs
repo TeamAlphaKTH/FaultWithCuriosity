de62f49cc3f4cfd65b8721927734ac115744b267
sxkho <Serkan.anar24@gmail.com>
2023-05-11 10:54:23 +0200

    Hot fix, reset polaroid charges

--- Assets/Scripts/CreateMenuScript.cs
+++ Assets/Scripts/CreateMenuScript.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class CreateButtonScript:NetworkBehaviour {
+public class CreateButtonScript : NetworkBehaviour
+{
 
 	private TMP_InputField portInput;
 	private Button btn;
@@ -16,26 +17,34 @@ public class CreateButtonScript:NetworkBehaviour {
 	private string localIp;
 	private ushort port;
 
-	private void Start() {
+	private void Start()
+	{
 		portInput = GameObject.Find("/Main Menu/CreateGameMenu").GetComponentInChildren<TMP_InputField>();
 		btn = GameObject.Find("/Main Menu/CreateGameMenu").GetComponentInChildren<Button>();
-		btn.onClick.AddListener(() => {
+		btn.onClick.AddListener(() =>
+		{
 			OnButtonPress();
 		});
 	}
-	public void OnButtonPress() {
+	public void OnButtonPress()
+	{
 		//Find local ips using the [port]
 		localIp = GetLocalIpadress();
 		port = 7777;
 
 		//Parse the port text into a ushort 
-		if(portInput.text != "") {
-			try {
+		if (portInput.text != "")
+		{
+			try
+			{
 				port = ushort.Parse(portInput.text);
-				if(port < 0 || port > 65535) {
+				if (port < 0 || port > 65535)
+				{
 					port = 7777;
 				}
-			} catch(FormatException) {
+			}
+			catch (FormatException)
+			{
 				Debug.Log("Portnumber format error. Default to 7777");
 				return;
 			}
@@ -43,7 +52,8 @@ public class CreateButtonScript:NetworkBehaviour {
 		portInput.text = localIp + ":" + port;
 
 		//Logic for when port is correct
-		if(!wrongFormat) {
+		if (!wrongFormat)
+		{
 			//Load the scene, the EventHandler will start hosting
 			CameraMovement.CanRotate = true;
 			FirstPersonController.CanMove = true;
@@ -52,22 +62,27 @@ public class CreateButtonScript:NetworkBehaviour {
 			Inventory.drugNr = 0;
 			Inventory.batteryNr = 0;
 			Flashlight.batteryLevel = 100;
+			PhotoCapture.charges = 3;
 			SceneManager.sceneLoaded += SceneManager_sceneLoaded;
 			SceneManager.LoadScene("Dungeon");
 		}
 		wrongFormat = false;
 	}
-	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
-		if(arg0.name.Equals("Dungeon")) {
+	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+		if (arg0.name.Equals("Dungeon"))
+		{
 			SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
 			NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(localIp, port, localIp);
 			NetworkManager.Singleton.StartHost();
 		}
 	}
-	public static string GetLocalIpadress() {
+	public static string GetLocalIpadress()
+	{
 		IPHostEntry ips = Dns.GetHostEntry(Dns.GetHostName());
-		foreach(var ip in ips.AddressList) {
-			if(ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
+		foreach (var ip in ips.AddressList)
+		{
+			if (ip.AddressFamily == AddressFamily.InterNetwork && !ip.ToString().EndsWith("1"))
 				return ip.ToString();
 		}
 		throw new Exception("Could not find current (if any) connected IPv4 address");
--- Assets/Scripts/SinglePlayerBtn.cs
+++ Assets/Scripts/SinglePlayerBtn.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SinglePlayerBtn:NetworkBehaviour {
+public class SinglePlayerBtn : NetworkBehaviour
+{
 
 	private Button btn;
 	private string Ip = "127.0.0.1";
 	private ushort port = 7777;
 
-	private void Start() {
+	private void Start()
+	{
 		btn = GameObject.Find("/Main Menu/PlayMenu/SinglePlayer Button").GetComponent<Button>();
-		btn.onClick.AddListener(() => {
+		btn.onClick.AddListener(() =>
+		{
 			OnButtonPress();
 		});
 	}
 
-	public void OnButtonPress() {
+	public void OnButtonPress()
+	{
 		CameraMovement.CanRotate = true;
 		FirstPersonController.CanMove = true;
 		PhotoCapture.canUseCamera = true;
@@ -25,12 +29,15 @@ public class SinglePlayerBtn:NetworkBehaviour {
 		Inventory.drugNr = 0;
 		Inventory.batteryNr = 0;
 		Flashlight.batteryLevel = 100;
+		PhotoCapture.charges = 3;
 		SceneManager.sceneLoaded += SceneManager_sceneLoaded;
 		SceneManager.LoadScene("Dungeon");
 	}
 
-	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
-		if(arg0.name.Equals("Dungeon")) {
+	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+		if (arg0.name.Equals("Dungeon"))
+		{
 			SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
 			NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(Ip, port, Ip);
 			NetworkManager.Singleton.StartHost();
